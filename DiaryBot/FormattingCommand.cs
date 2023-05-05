97cba6428a64db7112d2e2379a1fef90eeb410b4
Roman Koliadenko <romankasp1x@gmail.com>
2023-05-05 17:56:54 +0300

    Fix RichTextBox's custom context menu and formatting execution

--- DiaryBot/FormattingCommand.cs
+++ DiaryBot/FormattingCommand.cs
@@ -4,9 +4,15 @@ namespace DiaryBot
 {
     public static class FormattingCommand
     {
-        public static readonly RoutedCommand SetStrikethroughTextCmd = 
-            new RoutedUICommand("Strikethrough", "Strikethrough", typeof(FormattingCommand), new InputGestureCollection { new KeyGesture(Key.X, ModifierKeys.Control | ModifierKeys.Shift) });
-        public static readonly RoutedCommand SetSpoilerTextCmd = 
-            new RoutedUICommand("Spoiler", "Spoiler", typeof(FormattingCommand), new InputGestureCollection { new KeyGesture(Key.P, ModifierKeys.Control | ModifierKeys.Shift) });
+        public static readonly RoutedUICommand SetBoldTextCmd =
+            new("Bold", "Bold", typeof(FormattingCommand), new InputGestureCollection { new KeyGesture(Key.B, ModifierKeys.Control) });
+        public static readonly RoutedUICommand SetItalicTextCmd =
+            new("Italic", "Italic", typeof(FormattingCommand), new InputGestureCollection { new KeyGesture(Key.I, ModifierKeys.Control) });
+        public static readonly RoutedUICommand SetUnderlineTextCmd =
+            new("Underline", "Underline", typeof(FormattingCommand), new InputGestureCollection { new KeyGesture(Key.U, ModifierKeys.Control) });
+        public static readonly RoutedUICommand SetStrikethroughTextCmd =
+            new("Strikethrough", "Strikethrough", typeof(FormattingCommand), new InputGestureCollection { new KeyGesture(Key.X, ModifierKeys.Control | ModifierKeys.Shift) });
+        public static readonly RoutedUICommand SetSpoilerTextCmd =
+            new("Spoiler", "Spoiler", typeof(FormattingCommand), new InputGestureCollection { new KeyGesture(Key.P, ModifierKeys.Control | ModifierKeys.Shift) });
     }
 }
